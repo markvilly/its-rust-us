cfaf3a97153cf0bc5d080e7ce75bf122aa8939a7
Mark Paul <markpaulbusiness66@gmail.com>
2025-03-15 23:34:04 +0300

    Ownership and Data types

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,34 @@
 fn main() {
     
     // Data types
-    let mut int_1: i32 = 10; // (2 ^ 32 - 1) / 2
+
+    // signed integer -> i32, i8, i16, i24, 
+    // let mut int_1: i32 = 10; // (2 ^ 32 - 1) / 2
 
     // unsigned interger. 
-    let uint_2: u32 = 110; // (2 ^ 32 - 1)
-    let boolean: bool = false;
-    let char1: char = 'a';
-    let float_1: f64 = 10.4;
-    let string_slice_1: &str = "Hello";
+    // let uint_2: u32 = 110; // (2 ^ 32 - 1)
+    // let boolean: bool = false;
+    // let char1: char = 'a';
+    // let float_1: f64 = 10.4;
+    // let string_slice_1: &str = "Hello";
 
-    let string_1:  String = String::from("Hello hello");
+    // let string_1:  String = String::from("Hello hello");
 
     // Immutable and Mutables
 
-    int_1 = 23;
+    // int_1 = 23;
 
     let a: i32 = 10;
     let b:i32 = 15;
     let c:i32 = add(a,b);
 
-    println!("The sum of {} and {} is {}", a, b, c)
+    println!("The sum of {} and {} is {}", a, b, c);
+
+    let variable_1: String = String::from("Ayo");
+    let variable_2: &String = &variable_1;
+
+    println!("{}", variable_2)
+
     // Functions  
 
 }
@@ -31,8 +39,9 @@ fn add(a: i32, b:i32)-> i32 {
     c
 }
 
-fn add_1(a: i32, b: i32) -> i32{
+// fn add_1(a: i32, b: i32) -> i32{
 
-    a + b
+//     a + b
 
-}
+// }
+
